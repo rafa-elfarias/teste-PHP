51774196d92494afeb24f25f04550918ddd56b98
rafaelrj <rafael.ribeirorj@gmail.com>
2022-05-15 20:49:45 -0300

    Update ArraySorteado.php

--- ArraySorteado.php
+++ ArraySorteado.php
@@ -1,11 +1,4 @@
 <?php
-/*Escreva um programa que preencha um array com 20 números inteiros sorteados entre 1 e 10. Depois informe qual ou quais número(s) não se repetiram.
-
-Exemplo
-
-Array sorteado = [2,5,8,2,8,5,3,9,6,3,4,6,3,1,2,1,2,3,7,1]
-Os números que não se repetem são o 4 e 7. */
-
 //Array com 20 números inteiros sorteados entre 1 e 10
 $arr = array();
 for($i=0; $i<20; $i++){
